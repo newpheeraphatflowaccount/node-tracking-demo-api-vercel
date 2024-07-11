6a3dbcbc76a0eaf1ae8b2f938f457884536213dc
Pheeraphat Prisan <pheeraphat_p@flowaccount.com>
2024-07-11 15:53:07 +0700

    feat: add another api route

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const axios = require('axios')
-const app = express()
-const PORT = 4000
+const axios = require('axios');
+const express = require('express');
+const app = express();
+const PORT = 4000;
 
 const email = 'pheeraphat_p@flowaccount.com'; 
-const apiUrl = 'https://api.hubapi.com/crm/v3/objects/contacts/search';
+const searchApiUrl = 'https://api.hubapi.com/crm/v3/objects/contacts/search';
+const contactDetailsApiUrl = 'https://api.hubapi.com/crm/v3/objects/contacts/';
 const bearerToken = 'pat-na1-577d3958-9ac8-4d6e-af18-b2f04799d96e';
 
 const config = {
@@ -28,39 +29,61 @@ const body = {
   ],
 };
 
-app.get('/search-contact-by-id', (req, res) => {
-  axios.post(apiUrl, body, config)
-    .then(response => {
-      res.json(response.data);
-    })
-    .catch(error => {
-      if (error.response) {
-        res.status(error.response.status).json({
-          message: 'Server-side error',
-          details: error.response.data,
-        });
-      } else if (error.request) {
-        res.status(500).json({
-          message: 'No response received from the server',
-          details: error.request,
-        });
-      } else {
-        res.status(500).json({
-          message: 'Error message',
-          details: error.message,
-        });
-      }
-    });
+
+const searchContact = async () => {
+  try {
+    const response = await axios.post(searchApiUrl, body, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getContactDetails = async (hsObjectId) => {
+  try {
+    const response = await axios.get(`${contactDetailsApiUrl}${hsObjectId}?associations=MEETING`, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.get('/search-contact', async (req, res) => {
+  try {
+    const searchResult = await searchContact();
+    if (searchResult.total > 0) {
+      const hsObjectId = searchResult.results[0].properties.hs_object_id;
+      const contactDetails = await getContactDetails(hsObjectId);
+      res.json({ searchResult, contactDetails });
+    } else {
+      res.status(404).json({ message: 'No contact found with the given email' });
+    }
+  } catch (error) {
+    if (error.response) {
+      res.status(error.response.status).json({
+        message: 'Server-side error',
+        details: error.response.data,
+      });
+    } else if (error.request) {
+      res.status(500).json({
+        message: 'No response received from the server',
+        details: error.request,
+      });
+    } else {
+      res.status(500).json({
+        message: 'Error message',
+        details: error.message,
+      });
+    }
+  }
 });
 
 
 app.get('/', (req, res) => {
-  res.send('This is my API running')
-})
+  res.send('This is my API running');
+});
+
 
 app.listen(PORT, () => {
-  console.log(`API Listening on PORT ${PORT}`)
-})
-
-
-module.exports = app
+  console.log(`API Listening on PORT ${PORT}`);
+});
