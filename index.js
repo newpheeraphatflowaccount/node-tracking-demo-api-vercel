be2161ac43e3f74aae01d6796f25b6a2069070ef
Pheeraphat Prisan <pheeraphat_p@flowaccount.com>
2024-07-11 15:36:39 +0700

    feat: first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const PORT = 4000
+
+app.listen(PORT, () => {
+  console.log(`API Listening on PORT ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('This is my API running')
+})
+
+module.exports = app
