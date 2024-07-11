2fabc2c4c067b03b51497d8b9af2d5a37a124687
Pheeraphat Prisan <pheeraphat_p@flowaccount.com>
2024-07-11 16:00:08 +0700

    feat: add last one api

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const PORT = 4000;
 
 const email = 'pheeraphat_p@flowaccount.com'; 
 const searchApiUrl = 'https://api.hubapi.com/crm/v3/objects/contacts/search';
-const contactDetailsApiUrl = 'https://api.hubapi.com/crm/v3/objects/contacts/';
+const contactApiUrl = 'https://api.hubapi.com/crm/v3/objects/contacts';
+const meetingApiUrl = 'https://api.hubapi.com/crm/v3/objects/MEETING/batch/read';
 const bearerToken = 'pat-na1-577d3958-9ac8-4d6e-af18-b2f04799d96e';
 
 const config = {
@@ -15,7 +16,7 @@ const config = {
   },
 };
 
-const body = {
+const searchBody = {
   filterGroups: [
     {
       filters: [
@@ -29,34 +30,54 @@ const body = {
   ],
 };
 
+const meetingProperties = [
+  "hs_meeting_start_time",
+  "hs_meeting_end_time",
+  "hs_meeting_outcome",
+  "hs_meeting_title",
+  "hs_meeting_external_url",
+  "hs_timestamp",
+  "hubspot_owner_id",
+  "hs_meeting_body",
+  "hs_internal_meeting_notes",
+  "hs_meeting_location",
+  "hs_activity_type",
+  "hs_attachment_ids"
+];
 
-const searchContact = async () => {
+app.get('/search-contact-by-id', async (req, res) => {
   try {
-    const response = await axios.post(searchApiUrl, body, config);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const getContactDetails = async (hsObjectId) => {
-  try {
-    const response = await axios.get(`${contactDetailsApiUrl}${hsObjectId}?associations=MEETING`, config);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-app.get('/search-contact', async (req, res) => {
-  try {
-    const searchResult = await searchContact();
-    if (searchResult.total > 0) {
-      const hsObjectId = searchResult.results[0].properties.hs_object_id;
-      const contactDetails = await getContactDetails(hsObjectId);
-      res.json({ searchResult, contactDetails });
+    // Search for contact by email
+    const searchResponse = await axios.post(searchApiUrl, searchBody, config);
+    const results = searchResponse.data.results;
+    
+    if (results.length > 0) {
+      const hsObjectId = results[0].properties.hs_object_id;
+      
+      // Get contact details including associated meetings
+      const contactResponse = await axios.get(`${contactApiUrl}/${hsObjectId}?associations=MEETING`, config);
+      const contactData = contactResponse.data;
+      
+      // Extract meeting IDs from associations
+      const meetingIds = contactData.associations.meetings.results.map(meeting => ({ id: meeting.id }));
+      
+      if (meetingIds.length > 0) {
+        // Prepare the body for the meeting details request
+        const meetingBody = {
+          properties: meetingProperties,
+          inputs: meetingIds
+        };
+        
+        // Get details of the meetings
+        const meetingResponse = await axios.post(meetingApiUrl, meetingBody, config);
+        
+        // Send the meeting details as the response
+        res.json(meetingResponse.data);
+      } else {
+        res.status(404).json({ message: 'No meetings associated with this contact' });
+      }
     } else {
-      res.status(404).json({ message: 'No contact found with the given email' });
+      res.status(404).json({ message: 'Contact not found' });
     }
   } catch (error) {
     if (error.response) {
@@ -78,12 +99,12 @@ app.get('/search-contact', async (req, res) => {
   }
 });
 
-
 app.get('/', (req, res) => {
   res.send('This is my API running');
 });
 
-
 app.listen(PORT, () => {
   console.log(`API Listening on PORT ${PORT}`);
 });
+
+module.exports = app;
